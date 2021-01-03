612dc9ac71498ed7c6ad98dd119026f7056df94f
Marcus Nilszén <mebn122@gmail.com>
2021-01-03 19:14:47 +0100

    draws enemies on screen with data from server!

--- client.py
+++ client.py
@@ -4,10 +4,14 @@ from network import Network
 import dimensions
 
 # Clears screen and redraws elements.
-def redraw_window(win, player1, player2):
+def redraw_window(win, player1, player2, enemies_pos):
     win.fill((255, 255, 255))
     player1.draw(win)
     player2.draw(win)
+
+    for enemy_pos in enemies_pos:
+        x, y = enemy_pos
+        pygame.draw.rect(win, (0,0,255), (x, y, dimensions.ENEMY_WIDTH, dimensions.ENEMY_HEIGHT)) # should be a Enemy class or something
 
     pygame.display.update()
 
@@ -21,8 +25,8 @@ def update_players(network, player1, player2):
         player1.update_pos(player1_pos)
         player2.update_pos(player2_pos)
     
-    player1.update_draw()
-    player2.update_draw()
+    player1.update_rect()
+    player2.update_rect()
 
 
 def main():
@@ -41,8 +45,10 @@ def main():
     while is_running:
         clock.tick(60)
 
+        enemies_pos = network.get_enemies_pos()
         update_players(network, player1, player2)
-        redraw_window(win, player1, player2)
+        redraw_window(win, player1, player2, enemies_pos)
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
