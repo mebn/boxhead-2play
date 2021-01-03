b2538c492dac00d8893fe8485cb58b72ca4b5081
Marcus Nilszén <mebn122@gmail.com>
2021-01-03 19:12:20 +0100

    generate enemies and can send enemies pos to clients

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import dimensions
 import codes
+import random
 
 ADDRESS = "127.0.0.1"
 PORT = 8888
@@ -31,7 +32,7 @@ class Entity:
     # Takes a list with bools and player (1 or 2).
     # Moves the player on server side.
     # Includes wall detection.
-    def update_pos(self, direction):
+    def update_player_pos(self, direction):
         if self.type != "player":
             return
 
@@ -56,31 +57,79 @@ class Entity:
         self.pos = [self.x, self.y]
 
 
+    def update_enemy_pos(self, players):
+        if self.type != "enemy":
+            return
+
+        movement_speed = 1 # less then player m. speed
+
+        # enemies move toward the closest player.
+        # handle movement in x direction.
+        if self.x <= players[0].x or self.x <= players[1].x:
+            self.x += movement_speed
+        else:
+            self.x -= movement_speed
+        
+        # handle movement in y direction.
+        if self.y <= players[0].y or self.y <= players[1].y:
+            self.y += movement_speed
+        else:
+            self.y -= movement_speed
+
+        self.pos = [self.x, self.y]
+
+
+
+
+def generate_enemies(amount):
+    enemies_list = []
+    radius = 60
+
+    for _ in range(amount):
+        x = random.randint(0, dimensions.WIDTH)
+        y = random.randint(0, dimensions.HEIGHT)
+
+        # TODO: Spawn radius around players.
+        while [x, y] == players[0].pos or [x, y] == players[1].pos:
+            x = random.randint(0, dimensions.WIDTH)
+            y = random.randint(0, dimensions.HEIGHT)
+
+        enemies_list.append(Entity("enemy", [x, y], 100))
+
+    return enemies_list
+
+
 players = [
     Entity("player", [0, 0], 100),
     Entity("player", [400, 400], 100)
 ]
-enemies = [
-    Entity("enemy", [100, 200], 100),
-    Entity("enemy", [200, 100], 100)
-]
+enemies = generate_enemies(5) # starting game with 5 enemies.
 
-
-# Returns "x1,y1;x2,y2" and makes sure every client is player 1.
-def all_pos_to_str(player):
+# Returns "x1,y1 x2,y2" and makes sure every client is player 1.
+def players_pos_to_str(player):
     player1_coords = players[0].get_pos_as_str()
     player2_coords = players[1].get_pos_as_str()
 
     if player == 0:
-        return f"{player1_coords};{player2_coords}"
+        return f"{player1_coords} {player2_coords}"
     else:
-        return f"{player2_coords};{player1_coords}"
+        return f"{player2_coords} {player1_coords}"
+
+
+# formattes enemies pos to a string like "x1 y1;x2 y2;x3 y3;"
+def enemies_pos_to_str(enemies):
+    pos_str = ""
+
+    for enemy in enemies:
+        pos_str += f"{enemy.x} {enemy.y};"
+
+    return pos_str
 
 
 # A thread that starts when a new clients connects.
 def client_thread(conn, player):
     # this only send one time, when connecting.
-    reply = all_pos_to_str(player).encode()
+    reply = players_pos_to_str(player).encode()
     conn.send(reply) # sends starting pos.
 
     while True:
@@ -95,12 +144,16 @@ def client_thread(conn, player):
 
             # handle movement
             if code == codes.player_movement:
-                players[player].update_pos(data)
-                reply = all_pos_to_str(player).encode()
+                players[player].update_player_pos(data)
+                reply = players_pos_to_str(player).encode()
                 conn.send(reply)
 
             if code == codes.enemies_position:
-                pass
+                for enemy in enemies:
+                    enemy.update_enemy_pos(players)
+
+                reply = enemies_pos_to_str(enemies).encode()
+                conn.send(reply)
 
             if code == codes.player_hp:
                 pass
